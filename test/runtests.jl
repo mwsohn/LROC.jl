838251617a1f61b5ecf49eb104b22d797afe1bc0
mwsohn <mwsohn@gmail.com>
2024-10-24 16:27:08 -0400

    Create runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+## runtests.jl  Unit tests for ROC
+## (c) 2014--2015 David A. van Leeuwen
+##
+## Licensed under the MIT software license, see LICENSE.md
+
+using DataFrames
+using ROCAnalysis
+using CSV
+
+
