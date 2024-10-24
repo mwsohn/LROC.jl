4814fa4c3c8e210044355e7732d7b898b944059b
mwsohn <mwsohn@gmail.com>
2024-10-23 22:59:31 -0400

    Update LROC.jl

--- src/LROC.jl
+++ src/LROC.jl
@@ -14,7 +14,11 @@ using ROCAnalysis, Plots, GLM
 ##
 ##############################################################################
 
-export rocinput, lroc, rocplot, roccomp
+export rocinput,    # creates target and nontarget vectors from logistic regression output suitable for input to ROCAnalysis.roc
+    lroc,            # outputs AUC value and optionally plot the ROC curves using Plots.jl
+    rocplot,        # produces ROC curve plot using Plots.jl
+    roccomp         # compares two ROC Curves from two logistic regressions using ROCAnalysis.delong_test
+
 
 ##############################################################################
 ##
